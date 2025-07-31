277916b846c4117a873faf773916426ece138fe3
Isaac Melo <isaacalvesx7@gmail.com>
2025-07-31 16:21:06 -0300

    ♻️ refactor: move state inside likebutton

--- src/components/photoCard/likeButton.tsx
+++ src/components/photoCard/likeButton.tsx
@@ -1,6 +1,7 @@
 import notliked from "../../assets/notliked.png";
 import liked from "../../assets/liked.png";
 import styled from "styled-components";
+import { useState } from "react";
 
 const Wrapper = styled.button`
   border: none;
@@ -13,13 +14,8 @@ const Wrapper = styled.button`
   }
 `;
 
-export default function LikeButton({
-  isLiked,
-  setIsLiked,
-}: {
-  isLiked: boolean;
-  setIsLiked: (isLiked: boolean) => void;
-}) {
+export default function LikeButton() {
+  const [isLiked, setIsLiked] = useState(false);
   return (
     <Wrapper onClick={() => setIsLiked(!isLiked)}>
       <img src={isLiked ? liked : notliked} alt="like-button" />
--- src/pages/all-photos/Photo/index.tsx
+++ src/pages/all-photos/Photo/index.tsx
@@ -1,13 +1,11 @@
-import { useState } from "react";
 import { PhotoCard } from "../../../components/photoCard";
 import type { Photo } from "../../../services/pexels/types";
 
 export default function PhotoComponent({ photo }: { photo: Photo }) {
   const { alt, avg_color, src, photographer, photographer_url } = photo;
-  const [isLiked, setIsLiked] = useState(false);
   return (
     <PhotoCard.Wrapper tabIndex={0} aria-label={alt}>
-      <PhotoCard.LikeButton isLiked={isLiked} setIsLiked={setIsLiked} />
+      <PhotoCard.LikeButton />
       <PhotoCard.Photo url={src.medium} alt={alt} />
       <PhotoCard.Details name={photographer} alt={alt} avg_color={avg_color} />
       <PhotoCard.PortfolioLink link={photographer_url} text={"Portfolio"} />
