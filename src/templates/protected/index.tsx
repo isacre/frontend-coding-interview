b0046a22a959c5dab2c73f5ccf4df35881c72ed5
Isaac Melo <isaacalvesx7@gmail.com>
2025-07-30 21:28:56 -0300

    ✨ feature: make protected template not show any html for unauthenticated users

--- src/templates/protected/index.tsx
+++ src/templates/protected/index.tsx
@@ -1,18 +1,21 @@
 import { Outlet, useNavigate } from "react-router";
 import { useAuthContext } from "../../utils/context/AuthContext/useAuthContext";
 import { useEffect } from "react";
+import { Wrapper } from "./styles";
 
 export default function ProtectedTemplate() {
   const { isAuthenticated, setIsAuthenticated } = useAuthContext();
   const navigate = useNavigate();
 
   useEffect(() => {
-    console.log(isAuthenticated);
-
     if (!isAuthenticated) {
       navigate("/");
     }
   }, [isAuthenticated, navigate, setIsAuthenticated]);
 
-  return isAuthenticated ? <Outlet /> : null;
+  return isAuthenticated ? (
+    <Wrapper>
+      <Outlet />
+    </Wrapper>
+  ) : null;
 }
---
+++ src/templates/protected/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+export const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+
+  @media (min-width: 1024px) {
+    width: 700px;
+    height: 100%;
+    margin: 0 auto;
+  }
+`;
